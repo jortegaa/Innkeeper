6d9e365bcff5141e8897522ebdf8f8043e1c893c
Jesús Ortega Ayala <jortegaa.dev@gmail.com>
2026-09-21 18:26:19 +0200

    Initial Commit

---
+++ Innkeeper/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Innkeeper
+//
+//  Created by Jesús Ortega Ayala on 21/09/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Innkeeper/InnkeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  InnkeeperApp.swift
+//  Innkeeper
+//
+//  Created by Jesús Ortega Ayala on 21/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct InnkeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
